fedb3010cea7fa677c3c917257d7cbf1d7cc2aef
Lucas Carvalho <killonferreira@gmail.com>
2024-06-30 02:42:44 -0300

    Ajustes iniciais com os links do Lucas

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("Light")) {
     //se estiver em Light mode, adicionar a imagem azul
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
-    img.setAttribute("alt", "Foto fundo azul")
+    img.setAttribute("src", "./assets/assets/Lucas-light.jpeg")
+    img.setAttribute("alt", "Foto guitarra")
   } else {
     //se estiver sem o Light mode mander a imagem amarela
-    img.setAttribute("src", "./assets/assets/avatar.png")
-    img.setAttribute("alt", "Foto de Mayk Brito fundo amarelo")
+    img.setAttribute("src", "./assets/assets/Lucas.jpeg")
+    img.setAttribute("alt", "Foto do Lucas ")
   }
 }
