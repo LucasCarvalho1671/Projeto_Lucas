07878acc1b7762ceade7d0181970eb6c22206dca
Lucas Carvalho <killonferreira@gmail.com>
2024-10-18 16:07:02 -0300

    Links

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  //controla o fundo
+  const html = document.documentElement
+  html.classList.toggle("Light")
+
+  //pegar a imagem
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("Light")) {
+    //se estiver em Light mode, adicionar a imagem azul
+    img.setAttribute("src", "./assets/assets/Lucas-light.jpeg")
+    img.setAttribute("alt", "Foto guitarra")
+  } else {
+    //se estiver sem o Light mode mander a imagem amarela
+    img.setAttribute("src", "./assets/assets/Lucas.png")
+    img.setAttribute("alt", "Foto do Lucas ")
+  }
+}
